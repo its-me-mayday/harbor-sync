8ca1ce91d760a3c4eded96dfe1c1036b0f5485c4
Luca Maggio <lucamaggio1992@gmail.com>
2024-11-28 16:15:06 +0100

    feat: delete useless mvc pattern and setup api rest pattern

--- controllers/harbor.py
+++ controllers/harbor.py
@@ -4,27 +4,28 @@ import requests
 logger = logging.getLogger("harbor_sync")
 
 class HarborController:
-    def __init__(self, model, view):
+    def __init__(self, model):
         self.model = model
-        self.view = view
 
     def get_repositories_by_project(self, project_name):
-        logger.info(f"Controller get_repositories_by_project starts using PROJECT: {project_name}")
+        logger.debug(f"Uses project_name: {project_name}")
+    
         url = f"{self.model.url}/api/v2.0/projects/{project_name}/repositories"
-        logger.info(f"Controller get_repositories_by_project uses URL: {url}")
+        logger.debug(f"Uses url: {url}")
         
         auth = (self.model.username, self.model.password)
-        logger.info(f"Controller get_repositories_by_project auth params by username: {self.model.username}")
+        logger.info(f"Auth params by username: {self.model.username}")
         
         try:
             response = requests.get(url, auth=auth)
-            logger.info(f"Controller get_repositories_by_project response: [status: {response.status_code}] {url}")
-            logger.debug(f"Controller get_repositories_by_project content response: {response.content}")
+            logger.info(f"Status response: {response.status_code} with url: {url}")
+            logger.debug(f"Content response: {response.content}")
             response.raise_for_status()
         except requests.exceptions.RequestException as e:
-            logger.error(f"Controller get_repositories_by_project encountered an error: {e}")
+            logger.error(f"Encountered a RequestException: {e}")
             raise
         except Exception as e:
-            logger.error(f"Controller get_repositories_by_project encountered an error: {e}")
+            logger.error(f"Encountered an Exception: {e}")
             raise
-        return response.json
+
+        return response.json()
--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 from config.logger import logger
 from config.settings import settings
 from models.registry import RegistryModel
-from views.cli import CliView
 from controllers.harbor import HarborController
+import os
 
 def main():
     logger.info("Run Harbor Sync application.")
-    
     logger.info(f"Using registry URL: {settings.REGISTRY_SRC}") 
 
     try:    
         model = RegistryModel(settings.REGISTRY_SRC, settings.USERNAME_SRC, settings.PASSWORD_SRC)
-        logger.info(f"RegistryModel correctly instatiated!")
-        view = CliView() 
-        logger.info(f"CliView correctly instatiated!")
-        controller = HarborController(model, view) 
+        logger.info(f"Model correctly instatiated!")
+        controller = HarborController(model) 
         logger.info(f"Controller correctly instatiated!")
 
         logger.info(f"Retrieve repositories for project: {settings.PROJECT_NAME}")
         repositories = controller.get_repositories_by_project(settings.PROJECT_NAME)
-        logger.info(f"Repositories found: {repositories}")
 
         if repositories:
             logger.info(f"Found {len(repositories)} repositories.")
-            view.display_repositories(repositories)
+            logger.info(f"Repositories: {repositories}")
         else:
             logger.warning("No repository found.")
     except Exception as e:
