2b982994764cdd2c81f6436091a53d7e73d12ba5
Luca Maggio <lucamaggio1992@gmail.com>
2024-11-28 11:49:25 +0100

    debug(main): setup logger.info to inform application starts

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     PASSWORD_SRC = "" # substitute with robot$harbor and setup a setting for this part (information hiding)
     PROJECT_NAME = "ccoe"
 
+    logger.info("Run Harbor Sync application.")
+
     try:    
         model = Registry(REGISTRY_SRC, USERNAME_SRC, PASSWORD_SRC)
         view = CliView() 
@@ -17,12 +19,12 @@ def main():
         repositories = controller.get_repositories_by_project(PROJECT_NAME)
 
         if repositories:
-            logger.info(f"Found {len(repositories)} repositories")
+            logger.info(f"Found {len(repositories)} repositories.")
             view.display_repositories(repositories)
         else:
-            logger.warning("No repository found")
+            logger.warning("No repository found.")
     except Exception as e:
-        logger.error(f"Error during application execution")
+        logger.error(f"Error during application execution.")
 
 if __name__ == "__main__":
     main()
