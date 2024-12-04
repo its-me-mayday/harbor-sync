5943ee3d340ccb2fb93be36e1440ff9b353f8f74
Luca Maggio <lucamaggio1992@gmail.com>
2024-12-04 11:17:04 +0100

    feat(Registry): setup getters

--- models/registry.py
+++ models/registry.py
@@ -1,8 +1,20 @@
 class Registry:
-    def __init__(self, url: str, username: str, password: str):
-        self._url = url
+    def __init__(self, host: str, username: str, password: str):
+        self._host = host
         self._username = username
+        self._password = password
+    
+    @property
+    def host(self):
+        self._host = host
+    
+    @property
+    def username(self):
+        self._username = username
+    
+    @property
+    def password(self):
         self._password = password
 
     def __repr__(self):
-      return f"Registry(url={self.url}, username={self.username}, password=****)"
+      return f"Registry(host={self.host}, username={self.username}, password=****)"
